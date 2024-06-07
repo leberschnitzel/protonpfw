b611545e78501deb822922263341f887ca6d7f71
leberschnitzel <git@nichts.existiert.ch>
2024-06-07 14:54:11 +0000

    Change output to just show the port and time

--- pfw.sh
+++ pfw.sh
@@ -2,6 +2,8 @@
 
 cd "$(python3 -m pip show py-natpmp | grep Location | cut -d\  -f 2)/natpmp" && while true ; do
   date
-  python3 natpmp_client.py -g 10.2.0.1 0 0 || { echo -e "ERROR with natpmpc command \a" ; break ; }
+  output=$(python3 natpmp_client.py -g 10.2.0.1 0 0) || { echo -e "ERROR with natpmpc command \a" ; break ; }
+  public_port=$(echo $output | grep -oP 'public port \K\d+')
+  echo Port forwarded: $public_port
   sleep 45
 done

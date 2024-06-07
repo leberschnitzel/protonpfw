ac37b97019d8e4d95b1c517a74a8959b97b53839
leberschnitzel <git@nichts.existiert.ch>
2024-06-07 12:45:32 +0000

    fixing code mistake, view from inside container

--- pfw.sh
+++ pfw.sh
@@ -5,6 +5,6 @@ cd "$(python3 -m pip show py-natpmp | grep Location | cut -d\  -f 2)/natpmp" && 
   output=$(python3 natpmp_client.py -g 10.2.0.1 0 0) || { echo -e "ERROR with natpmpc command \a" ; break ; }
   echo $output
   public_port=$(echo $output | grep -oP 'public port \K\d+')
-  sed -i "s/\"listen_ports\": \[[0-9]*, [0-9]*\],/\"listen_ports\": [$public_port, $public_port],/g" /your/deluge/config/core.conf
+  sed -i "s/\"listen_ports\": \[[0-9]*, [0-9]*\],/\"listen_ports\": [$public_port, $public_port],/g" /config/core.conf
   sleep 45
 done

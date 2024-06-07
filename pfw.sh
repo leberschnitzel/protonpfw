c88b9427248974116b5bc010cc4f3cfd5946e22a
leberschnitzel <github@nichts.existiert.ch>
2024-06-07 08:39:38 +0200

    Changing code to directly edit deluge config

--- pfw.sh
+++ pfw.sh
@@ -2,6 +2,9 @@
 
 cd "$(python3 -m pip show py-natpmp | grep Location | cut -d\  -f 2)/natpmp" && while true ; do
   date
-  python3 natpmp_client.py -g 10.2.0.1 0 0 || { echo -e "ERROR with natpmpc command \a" ; break ; }
+  output=$(python3 natpmp_client.py -g 10.2.0.1 0 0) || { echo -e "ERROR with natpmpc command \a" ; break ; }
+  echo $output
+  public_port=$(echo $output | grep -oP 'public port \K\d+')
+  sed -i "s/\"listen_ports\": \[[0-9]*, [0-9]*\],/\"listen_ports\": [$public_port, $public_port],/g" /your/deluge/config/core.conf
   sleep 45
 done

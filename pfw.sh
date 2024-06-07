95280384133cc55a197110e09ac179ea415b228d
leberschnitzel <git@nichts.existiert.ch>
2024-06-07 13:30:26 +0000

    Fixes to script

--- pfw.sh
+++ pfw.sh
@@ -3,8 +3,13 @@
 cd "$(python3 -m pip show py-natpmp | grep Location | cut -d\  -f 2)/natpmp" && while true ; do
   date
   output=$(python3 natpmp_client.py -g 10.2.0.1 0 0) || { echo -e "ERROR with natpmpc command \a" ; break ; }
-  echo $output
   public_port=$(echo $output | grep -oP 'public port \K\d+')
-  sed -i "s/\"listen_ports\": \[[0-9]*, [0-9]*\],/\"listen_ports\": [$public_port, $public_port],/g" /config/core.conf
+  echo $public_port
+  sed -i "/"listen_ports": \[/,/\]/c\
+      "listen_ports": [\
+          $public_port,\
+          $public_port\
+      ]," /config/core.conf
+
   sleep 45
 done
